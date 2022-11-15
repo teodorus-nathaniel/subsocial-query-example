2ae5a4a953c22f0128e9b385306da86c0b296f38
teodorus-nathaniel <nathankurnia26@gmail.com>
2022-11-15 19:27:57 +0700

    Add comment to setup subsocial to prod

--- src/services/client.ts
+++ src/services/client.ts
@@ -2,6 +2,15 @@ import { QueryClient } from '@tanstack/react-query'
 import { createQueryInvalidation, setSubsocialConfig } from 'subsocial-query1'
 
 setSubsocialConfig('staging')
+// setSubsocialConfig('prod', {
+//   postConnectConfig: (api) => {
+//     const mnemonic = '' // CRUST MNEMONIC
+//     const authHeader = generateCrustAuthToken(mnemonic)
+//     api.ipfs.setWriteHeaders({
+//       authorization: 'Basic ' + authHeader,
+//     })
+//   },
+// })
 
 const queryClient = new QueryClient({
   defaultOptions: {
